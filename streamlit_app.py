b135025d542f71e3ec4e7b4f26751b8e6712a695
nicolamassari <nicola.massari1705@gmail.com>
2024-09-21 21:04:20 +0200

    minor fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from geopy.distance import geodesic
-import seaborn as sns
 import geopandas as gpd
 
 import streamlit as st
